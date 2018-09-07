d81ec4d8e08919086e86b753d8d3cb04299815d7
Ali Mir <110abidi@gmail.com>
2018-09-06 19:27:52 -0700

    added more dues

--- Duas/Dua.swift
+++ Duas/Dua.swift
@@ -12,11 +12,13 @@ class Dua: Equatable {
     let englishName: String
     let arabicName: String
     let text: String
+    let category: String
     
-    init(englishName: String, arabicName: String, text: String) {
+    init(englishName: String, arabicName: String, text: String, category: String) {
         self.englishName = englishName
         self.arabicName = arabicName
         self.text = text
+        self.category = category
     }
     
     static func ==(lhs: Dua, rhs: Dua) -> Bool {
@@ -25,6 +27,23 @@ class Dua: Equatable {
 }
 
 let allDuas: [Dua] = [
-    Dua(englishName: "Kumayl", arabicName: "دعا کمیل", text: DuaTexts.kumayl),
-    Dua(englishName: "Mashlool", arabicName: "مشلول", text: DuaTexts.kumayl)
+    Dua(englishName: "Dua Kumayl", arabicName: "دعاء کمیل", text: DuaTexts.kumayl, category: "Regular"),
+    Dua(englishName: "Dua Mashlool", arabicName:"دعاء المشلول", text: DuaTexts.mashlool, category: "Regular"),
+    Dua(englishName: "Dua Tawassul", arabicName: "دعاء تَوَسّولْ", text: DuaTexts.tawassul, category: "Regular"),
+    Dua(englishName: "Dua Ahad", arabicName: "دعاء عَهْد", text: DuaTexts.kumayl, category: "Regular"),
+    Dua(englishName: "Dua Sabah", arabicName: "دعاء الصباح", text: DuaTexts.kumayl, category: "Regular"),
+    Dua(englishName: "Dua Adeelah", arabicName: "دعاء عديلا", text: DuaTexts.kumayl, category: "Regular"),
+    Dua(englishName: "Dua Faraj", arabicName: "دعاء فرج", text: DuaTexts.kumayl, category: "Regular"),
+    Dua(englishName: "Fajr", arabicName: "الفجر", text: DuaTexts.kumayl, category: "Daily Prayer"),
+    Dua(englishName: "Dhuhr", arabicName: "الظهر", text: DuaTexts.kumayl, category: "Daily Prayer"),
+    Dua(englishName: "Asr", arabicName: "العصر", text: DuaTexts.kumayl, category: "Daily Prayer"),
+    Dua(englishName: "Maghrib ", arabicName: "المغرب", text: DuaTexts.kumayl, category: "Daily Prayer"),
+    Dua(englishName: "Isha", arabicName: "العشاء", text: DuaTexts.kumayl, category: "Daily Prayer"),
+    Dua(englishName: "Saturday", arabicName: "السبت", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Sunday", arabicName: "الأحد", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Monday", arabicName: "الأثنين", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Tuesday", arabicName: "الثلاثاء", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Wednesday", arabicName: "الأربعاء", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Thursday", arabicName: "الخميس", text: DuaTexts.kumayl, category: "Days Of The Week"),
+    Dua(englishName: "Friday", arabicName: "الجمعه", text: DuaTexts.kumayl, category: "Days Of The Week"),
 ]
--- Duas/DuasListViewController.swift
+++ Duas/DuasListViewController.swift
@@ -14,23 +14,44 @@ protocol DuaSelectionDelegate: class {
 
 class DuasListViewController: UITableViewController {
     
-    var duas = [Dua]()
+    @IBOutlet var searchBar: UISearchBar!
+    
+    var duas = [[Dua]]()
+    var categories = [String]()
     var delegate: DuaSelectionDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        duas = allDuas
+        
+        
+        
+        let duaGroup = Dictionary(grouping: allDuas) { String($0.category) }
+        categories = duaGroup.keys.map {String($0)}
+        for category in categories {
+            if let duas = duaGroup[category] {
+                self.duas.append(duas)
+            }
+        }
+        
     }
     
     // MARK: - Table view setup
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return duas.count
+        return duas[section].count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section]
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "duaCell", for: indexPath)
-        let dua = duas[indexPath.row]
+        let dua = duas[indexPath.section][indexPath.row]
         cell.textLabel?.text = dua.englishName
         cell.detailTextLabel?.text = dua.arabicName
         return cell
@@ -39,7 +60,7 @@ class DuasListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         _ = navigationController?.popViewController(animated: true)
-        delegate?.didSelectDua(duas[indexPath.row])
+        delegate?.didSelectDua(duas[indexPath.section][indexPath.row])
     }
     
 }
